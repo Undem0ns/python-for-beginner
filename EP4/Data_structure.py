5c647db68c4e0a45c304780d742fe6f307b9d211
Undem0ns <andamannnira@kkumail.com>
2021-12-20 11:18:49 +0700

    add comment

--- EP4/Data_structure.py
+++ EP4/Data_structure.py
@@ -1,15 +1,16 @@
-# List
+# โครงสร้างของข้อมูล List
 print('**********Data structure | List**********')
+
+# ประกาศตัวแปร
 list = ['apple', 'banana', 'orange']
-print( list )
+print(type(list))
+print(list[0])
+print(type[-1])
+print(type[1])
 
 # List methods
 list.append('kiwi')
-
 list.remove('banana')
-
 list.reverse()
-
 list.sort()
-
 print( list )
